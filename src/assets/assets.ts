fd43510e8bb0198d0338d572046a5a2bd935d416
mcneil <lukemcneil61@gmail.com>
2019-05-24 22:48:25 -0500

    Made top down 2d movement work

--- src/assets/assets.ts
+++ src/assets/assets.ts
@@ -3,10 +3,10 @@ import dude from "~/assets/dude.png";
 import platform from "~/assets/platform.png";
 import sky from "~/assets/sky.png";
 import star from "~/assets/star.png";
-import backpackGuy from "~/assets/wyvernPrince.png";
+import vanguard from "~/assets/Vanguard_32.png";
 import backgrounds from "~/assets/backgrounds.png";
 import items from "~/assets/items.png";
 import sword from "~/assets/sword.png";
 
 
-export default { bomb, dude, platform, sky, star, backpackGuy, backgrounds, items, sword };
+export default { bomb, dude, platform, sky, star, vanguard, backgrounds, items, sword };
--- src/index.ts
+++ src/index.ts
@@ -1,178 +1,3 @@
-// import "phaser";
-// import assets from "~/assets/assets";
-
-// let player;
-// let stars;
-// let bombs;
-// let platforms;
-// let cursors;
-// let score = 0;
-// let gameOver = false;
-// let scoreText;
-
-// function preload() {
-//     this.load.image("sky", assets.sky);
-//     this.load.image("ground", assets.platform);
-//     this.load.image("star", assets.star);
-//     this.load.image("bomb", assets.bomb);
-//     this.load.spritesheet("dude", assets.dude, { frameWidth: 32, frameHeight: 48 });
-// }
-
-// function create() {
-//     //  A simple background for our game
-//     this.add.image(400, 300, "sky");
-
-//     //  The platforms group contains the ground and the 2 ledges we can jump on
-//     platforms = this.physics.add.staticGroup();
-
-//     //  Here we create the ground.
-//     //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
-//     platforms
-//         .create(400, 568, "ground")
-//         .setScale(2)
-//         .refreshBody();
-
-//     //  Now let's create some ledges
-//     platforms.create(600, 400, "ground");
-//     platforms.create(50, 250, "ground");
-//     platforms.create(750, 220, "ground");
-
-//     // The player and its settings
-//     player = this.physics.add.sprite(100, 450, "dude");
-
-//     //  Player physics properties. Give the little guy a slight bounce.
-//     player.setBounce(0.2);
-//     player.setCollideWorldBounds(true);
-
-//     //  Our player animations, turning, walking left and walking right.
-//     this.anims.create({
-//         key: "left",
-//         frames: this.anims.generateFrameNumbers("dude", { start: 0, end: 3 }),
-//         frameRate: 10,
-//         repeat: -1
-//     });
-
-//     this.anims.create({
-//         key: "turn",
-//         frames: [{ key: "dude", frame: 4 }],
-//         frameRate: 20
-//     });
-
-//     this.anims.create({
-//         key: "right",
-//         frames: this.anims.generateFrameNumbers("dude", { start: 5, end: 8 }),
-//         frameRate: 10,
-//         repeat: -1
-//     });
-
-//     //  Input Events
-//     cursors = this.input.keyboard.createCursorKeys();
-
-//     //  Some stars to collect, 12 in total, evenly spaced 70 pixels apart along the x axis
-//     stars = this.physics.add.group({
-//         key: "star",
-//         repeat: 11,
-//         setXY: { x: 12, y: 0, stepX: 70 }
-//     });
-
-//     stars.children.iterate(function(child) {
-//         //  Give each star a slightly different bounce
-//         child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
-//     });
-
-//     bombs = this.physics.add.group();
-
-//     //  The score
-//     scoreText = this.add.text(16, 16, "score: 0", { fontSize: "32px", fill: "#000" });
-
-//     //  Collide the player and the stars with the platforms
-//     this.physics.add.collider(player, platforms);
-//     this.physics.add.collider(stars, platforms);
-//     this.physics.add.collider(bombs, platforms);
-
-//     //  Checks to see if the player overlaps with any of the stars, if he does call the collectStar function
-//     this.physics.add.overlap(player, stars, collectStar, null, this);
-
-//     this.physics.add.collider(player, bombs, hitBomb, null, this);
-// }
-
-// function update() {
-//     if (gameOver) {
-//         return;
-//     }
-
-//     if (cursors.left.isDown) {
-//         player.setVelocityX(-160);
-
-//         player.anims.play("left", true);
-//     } else if (cursors.right.isDown) {
-//         player.setVelocityX(160);
-
-//         player.anims.play("right", true);
-//     } else {
-//         player.setVelocityX(0);
-
-//         player.anims.play("turn");
-//     }
-
-//     if (cursors.up.isDown && player.body.touching.down) {
-//         player.setVelocityY(-330);
-//     }
-// }
-
-// function collectStar(player, star) {
-//     star.disableBody(true, true);
-
-//     //  Add and update the score
-//     score += 10;
-//     scoreText.setText("Score: " + score);
-
-//     if (stars.countActive(true) === 0) {
-//         //  A new batch of stars to collect
-//         stars.children.iterate(function(child) {
-//             child.enableBody(true, child.x, 0, true, true);
-//         });
-
-//         var x = player.x < 400 ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
-
-//         var bomb = bombs.create(x, 16, "bomb");
-//         bomb.setBounce(1);
-//         bomb.setCollideWorldBounds(true);
-//         bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
-//         bomb.allowGravity = false;
-//     }
-// }
-
-// function hitBomb(player) {
-//     this.physics.pause();
-
-//     player.setTint(0xff0000);
-
-//     player.anims.play("turn");
-
-//     gameOver = true;
-// }
-
-// var config: Phaser.Types.Core.GameConfig = {
-//     type: Phaser.AUTO,
-//     width: 800,
-//     height: 600,
-//     physics: {
-//         default: "arcade",
-//         arcade: {
-//             gravity: { y: 300 },
-//             debug: false
-//         }
-//     },
-//     scene: {
-//         preload: preload,
-//         create: create,
-//         update: update
-//     }
-// };
-
-// new Phaser.Game(config);
-
 import "phaser";
 import assets from "~/assets/assets";
 
@@ -190,7 +15,6 @@ var config: Phaser.Types.Core.GameConfig = {
   physics: {
     default: "arcade",
     arcade: {
-      gravity: { y: 1800 },
       debug: false
     }
   },
@@ -202,36 +26,33 @@ var config: Phaser.Types.Core.GameConfig = {
 };
 
 function preload() {
-  this.load.image("sword", assets.sword);
-  this.load.image("background", assets.backgrounds);
-  this.load.spritesheet("hero", assets.backpackGuy, {
+  this.load.spritesheet("hero", assets.vanguard, {
     frameWidth: 32,
     frameHeight: 32
   });
-  this.load.spritesheet("items", assets.items, {
-    frameWidth: 16,
-    frameHeight: 16
-  });
 }
 
+let up;
+let down;
+let left;
+let right;
 function create() {
+  up = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
+  down = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
+  left = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+  right = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
 
   cursors = this.input.keyboard.createCursorKeys();
-
-  platforms = this.physics.add.staticGroup();
-  platforms.create(-100, 450, "sword");
-  platforms.create(250, 750, "sword");
-  platforms.create(750, 450, "sword");
-
   player = this.physics.add.sprite(100, 20, "hero");
   player.setDisplaySize(64, 64);
   player.setCollideWorldBounds(true);
   player.setBounce(0.5);
   player.body.setGravityY(1.7);
-  this.physics.add.collider(player, platforms);
+
 
   this.anims.create({
-    key: "left",
+
+    key: "right",
     frames: this.anims.generateFrameNumbers("hero", {
       frames: [3, 4, 3, 5]
     }),
@@ -239,7 +60,7 @@ function create() {
     repeat: -1
   });
   this.anims.create({
-    key: "right",
+    key: "left",
     frames: this.anims.generateFrameNumbers("hero", {
       frames: [6, 7, 6, 8]
     }),
@@ -247,60 +68,72 @@ function create() {
     repeat: -1
   });
   this.anims.create({
-    key: "faceFront",
-    frames: [{ key: "hero", frame: 0 }],
-    frameRate: 20
+    key: "up",
+    frames: this.anims.generateFrameNumbers("hero", {
+      frames: [9, 10, 9, 11]
+    }),
+    frameRate: 10,
+    repeat: -1
   });
-
-  let items = this.physics.add.group({
-    key: "items",
-    repeat: 8,
-    setXY: { x: 12, y: 0, stepX: 70 }
-  });
-
-  items.children.iterate(function (child) {
-    child.setBounceY(.9);
-    child.setDisplaySize(32, 32);
-  });
-
-  this.physics.add.collider(platforms, items);
-  this.physics.add.overlap(player, items, getItem, null, this);
-
-  var score = 0;
-  var scoreText;
-  function getItem(player, item) {
-    item.disableBody(true, true);
-    score += 1;
-    scoreText.setText("score: " + score);
-    if (items.countActive(true) === 0) {
-      console.log("hi");
-      items.children.iterate(function (child) {
-        child.enableBody(true, child.x, 0, true, true);
-      });
-    }
-  }
-  scoreText = this.add.text(10, 10, "score: 0", {
-    fontSize: "32px",
-    fill: "#FFF"
+  this.anims.create({
+    key: "down",
+    frames: this.anims.generateFrameNumbers("hero", {
+      frames: [0, 1, 0, 2]
+    }),
+    frameRate: 10,
+    repeat: -1
   });
 }
 
+let directions = [];
+
 function update() {
-  if (cursors.left.isDown) {
-    player.setVelocityX(-225);
-
-    player.anims.play("left", true);
-  } else if (cursors.right.isDown) {
-    player.setVelocityX(225);
-
-    player.anims.play("right", true);
-  } else {
-    player.setVelocityX(0);
-    player.anims.play("faceFront", true);
+  if (Phaser.Input.Keyboard.JustDown(up)) {
+    directions.push("up");
   }
-
-  if (cursors.up.isDown && player.body.touching.down) {
-    player.setVelocityY(-630);
+  if (Phaser.Input.Keyboard.JustDown(down)) {
+    directions.push("down");
+  }
+  if (Phaser.Input.Keyboard.JustDown(left)) {
+    directions.push("left");
+  }
+  if (Phaser.Input.Keyboard.JustDown(right)) {
+    directions.push("right");
+  }
+  if (Phaser.Input.Keyboard.JustUp(up)) {
+    directions = directions.filter(d => d !== "up");
+  }
+  if (Phaser.Input.Keyboard.JustUp(down)) {
+    directions = directions.filter(d => d !== "down");
+  }
+  if (Phaser.Input.Keyboard.JustUp(left)) {
+    directions = directions.filter(d => d !== "left");
+  }
+  if (Phaser.Input.Keyboard.JustUp(right)) {
+    directions = directions.filter(d => d !== "right");
+  }
+  player.setVelocity(0);
+  if (directions.length === 0) {
+    player.anims.stop();
+  } else {
+    switch (directions[directions.length - 1]) {
+      case "up":
+        player.setVelocityY(-300);
+        player.anims.play("up", true)
+        break;
+      case "down":
+        player.setVelocityY(300);
+        player.anims.play("down", true)
+        break;
+      case "left":
+        player.setVelocityX(-300);
+        player.anims.play("left", true)
+        break;
+      case "right":
+        player.setVelocityX(300);
+        player.anims.play("right", true)
+        break;
+    }
   }
 }
 
