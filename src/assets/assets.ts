3aa59699c37c323b5d2a36e52a5b4bad6c4c8e27
Luke McNeil <lukemcneil61@gmail.com>
2019-08-21 10:49:36 -0500

    move server into separate repo

---
+++ src/assets/assets.ts
@@ -0,0 +1,25 @@
+import bomb from "~/assets/bomb.png";
+import dude from "~/assets/dude.png";
+import platform from "~/assets/platform.png";
+import sky from "~/assets/sky.png";
+import star from "~/assets/star.png";
+import vanguard from "~/assets/Vanguard_32.png";
+import backgrounds from "~/assets/backgrounds.png";
+import items from "~/assets/items.png";
+import sword from "~/assets/sword.png";
+import watchkeeper from "~/assets/watchkeeper.png";
+import waywalker from "~/assets/waywalker.png";
+
+export default {
+    waywalker,
+    watchkeeper,
+    bomb,
+    dude,
+    platform,
+    sky,
+    star,
+    vanguard,
+    backgrounds,
+    items,
+    sword
+};
